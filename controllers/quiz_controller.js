3c0a655b71ac1f23fade88f713bbbd4c6c5e9547
carlosmorenosanchez <carlos.moreno.sanchez@alumnos.upm.es>
2017-06-04 17:36:23 +0200

    arreglo5 p52

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -266,14 +266,14 @@ exports.randomcheck = function (req, res, next) {
     } else{
 
         var score = req.session.p52.pyp.length-1;
-        score.INTEGER;
+        
         delete req.session.p52;
 
         res.render('quizzes/randomresult', {
             quiz: req.quiz,
             result: result,
             answer: answer,
-            score: score
+            score: 0
         });
 
     }
