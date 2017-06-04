026fc551552609396cc21059f2bedf2e47545abb
carlosmorenosanchez <carlos.moreno.sanchez@alumnos.upm.es>
2017-06-04 13:53:30 +0200

    arreglo practica52

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -265,15 +265,14 @@ exports.randomcheck = function (req, res, next) {
 
     } else{
 
-        var score = req.session.p52.pyp.length-1;
-
+        
         delete req.session.p52;
 
         res.render('quizzes/randomresult', {
             quiz: req.quiz,
             result: result,
             answer: answer,
-            score: score
+            score: 0
         });
 
     }
